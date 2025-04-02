2416791c5876a7cd72b7419f5ffb1d14e8663385
bbwinner <bbcam7392@gmail.com>
2025-04-02 20:22:09 +0800

    change print_opt() and priority()

--- solution.c
+++ solution.c
@@ -17,12 +17,12 @@ void push(Stack* S, long long val){
     S->data[S->top] = val;
 }
 
-int pop(Stack* S){
+long long pop(Stack* S){
     (S->top)--;
     return S->data[(S->top)+1];
 }
 
-int top(Stack* S){
+long long top(Stack* S){
     return S->data[S->top];
 }
 
@@ -31,11 +31,7 @@ bool empty(Stack* S){
 }
 
 void print_opt(long long o){
-    if(o==-10) putchar('-');
-    else if(o==-9) putchar('+');
-    else if(o==-6) putchar('%');
-    else if(o==-5) putchar('/');
-    else if(o==-4) putchar('*');
+    printf("%c", (char) (o+48));
 }
 
 long long convert_opt(long long o){
@@ -43,11 +39,11 @@ long long convert_opt(long long o){
 }
 
 long long compute(long long a, long long b, long long o){
-    if(o==-10) return a-b;
-    if(o==-9) return a+b;
-    if(o==-6) return a%b;
-    if(o==-5) return a/b;
-    if(o==-4) return a*b;
+    if(o == -1) return a / b;
+    if(o == -6) return a * b;
+    if(o == -11) return a % b;
+    if(o == -3) return a - b;
+    if(o == -5) return a + b;
 }
 
 long long priority(long long o){
